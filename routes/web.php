20af67f2bd832c94a47724dd0d49b075631172c5
ihza rizky <ihzarizky30@gmail.com>
2021-07-27 14:17:20 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/auth/callback","AuthController@callback");
+Route::get("/auth/google","AuthController@google");
+Route::get("/auth","AuthController@index");
